0964b3e4209ac97221e5d761c25354e65c44ce22
Mikhail Maliugin <ghastmisha089@gmail.com>
2024-10-29 12:22:38 +0200

    errors replaced with custom error type

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -1,3 +1,14 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ProcessorError {
+    #[error("Invalid register number {0}, expected a number between 0 and 31")]
+    InvalidRegister(u64),
+
+    #[error("Cannot set register 0")]
+    CannotSetRegisterZero,
+}
+
 /// The `Processor` struct represents a simple risc-v 64i CPU
 #[derive(Debug, Clone, Copy)]
 pub struct Processor {
@@ -15,23 +26,23 @@ impl Processor {
     }
 
     /// Get the value of a register, private function because is to be used only in the module
-    fn get_register(&self, reg: u64) -> Result<u64, &str> {
+    fn get_register(&self, reg: u64) -> Result<u64, ProcessorError> {
         if reg == 0 {
             return Ok(0);
         }
         if reg > 31 {
-            return Err("Invalid register number");
+            return Err(ProcessorError::InvalidRegister(reg));
         }
         Ok(self.registers[(reg - 1) as usize])
     }
 
     /// Set the value of a register, private function because is to be used only in the module
-    fn set_register(&mut self, reg: u64, value: u64) -> Result<(), &str> {
+    fn set_register(&mut self, reg: u64, value: u64) -> Result<(), ProcessorError> {
         if reg == 0 {
-            return Err("Cannot set register 0");
+            return Err(ProcessorError::CannotSetRegisterZero);
         }
         if reg > 31 {
-            return Err("Invalid register number");
+            return Err(ProcessorError::InvalidRegister(reg));
         }
         self.registers[(reg - 1) as usize] = value;
         Ok(())
