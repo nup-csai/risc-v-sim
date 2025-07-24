99c7195da42c379be688922068d628cc7d6ef530
InnocentusLime <innocentuslime.help@gmail.com>
2025-07-24 19:34:12 +0300

    add kernel struct
    To properly separate the kernel from the outside code, group
    all the entities into one struct with a fitting API.

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -5,3 +5,118 @@ pub mod processor;
 pub use decoder::*;
 pub use instruction::*;
 pub use processor::*;
+use thiserror::Error;
+
+#[derive(Debug)]
+pub struct Kernel {
+    program: Program,
+    program_offset: u64,
+    processor: Processor,
+}
+
+impl Kernel {
+    pub fn new(program: Program, entry_point: u64, program_offset: u64) -> Self {
+        let mut processor = Processor::new();
+        processor.pc = entry_point;
+
+        Self {
+            program,
+            program_offset,
+            processor,
+        }
+    }
+
+    pub fn step(&mut self) -> Result<KernelStep, KernelError> {
+        let old_processor = self.processor;
+        let instruction = self.fetch_instruction()?;
+
+        instruction
+            .execute(&mut self.processor)
+            .map_err(|instruction_error| KernelError::InstructionError {
+                instruction_address: self.processor.pc,
+                instruction_error,
+            })?;
+        self.processor.pc += 4;
+
+        Ok(KernelStep {
+            old_processor,
+            instruction,
+            new_processor: self.processor,
+        })
+    }
+
+    fn fetch_instruction(&self) -> Result<Instruction, KernelError> {
+        // TODO: handle underflow
+        let local_offset = self.processor.pc - self.program_offset;
+        let instruction_index = (local_offset / 4) as usize;
+
+        self.program
+            .instructions
+            .get(instruction_index)
+            .map(|x| *x)
+            .ok_or(KernelError::FetchOutOfRange(instruction_index))
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Program {
+    instructions: Vec<Instruction>,
+}
+
+impl Program {
+    pub fn from_instructions(instructions: Vec<Instruction>) -> Self {
+        Self { instructions }
+    }
+
+    pub fn from_raw_instructions(
+        instructions: impl IntoIterator<Item = u32>,
+    ) -> Result<Self, InstructionDecodeError> {
+        let instructions = instructions
+            .into_iter()
+            .enumerate()
+            .map(Self::decode_instruction)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Self::from_instructions(instructions))
+    }
+
+    fn decode_instruction(
+        (idx, code): (usize, u32),
+    ) -> Result<Instruction, InstructionDecodeError> {
+        match decode_instruction(code) {
+            Ok(instruction) => Ok(instruction),
+            Err(error) => Err(InstructionDecodeError {
+                instruction_idx: idx,
+                instruction_code: code,
+                error,
+            }),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Error)]
+#[error("Failed to encode instruction {instruction_idx}: {instruction_code:#x} is not a valid instruction")]
+pub struct InstructionDecodeError {
+    pub instruction_idx: usize,
+    pub instruction_code: u32,
+    #[source]
+    pub error: DecodeError,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct KernelStep {
+    pub old_processor: Processor,
+    pub new_processor: Processor,
+    pub instruction: Instruction,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Error)]
+pub enum KernelError {
+    #[error("Failed to execute instruction {instruction_address}: {instruction_error}")]
+    InstructionError {
+        instruction_address: u64,
+        instruction_error: InstructionError,
+    },
+    #[error("Tried to fetch an instruction out of range: {0}")]
+    FetchOutOfRange(usize),
+}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ mod kernel;
 use clap::Parser;
 use elf::endian::AnyEndian;
 use elf::ElfBytes;
-use kernel::decode_instruction;
-use kernel::DecodeError;
-use kernel::Processor;
-use kernel::{Instruction, InstructionError};
+use kernel::{InstructionDecodeError, Kernel, KernelError, Program};
 use thiserror::Error;
 
 /// MVP of RVsim!
@@ -20,7 +17,7 @@ struct Args {
 const MAX_STEPS: usize = 10;
 
 #[derive(Debug, Error)]
-enum RunError {
+enum ShellError {
     #[error("Failed to load the .elf file")]
     Load(#[source] std::io::Error),
     #[error("Failed to parse the .elf file")]
@@ -39,95 +36,51 @@ enum RunError {
     CompressedText,
     #[error("The .text section is not a multiple of 4")]
     UnalignedText,
-    #[error("Instruction number {instruction_n}: {instruction_val:#x} is not a valid instruction")]
-    BadInstruction {
-        instruction_n: usize,
-        instruction_val: u32,
-        #[source]
-        reason: DecodeError,
-    },
-    #[error("Tried to fetch an instruction out of range: {0}")]
-    FetchOutOfRange(usize),
-    #[error("Failed to execute instruction {instruction_idx}: {instruction_error}")]
-    InstructionError {
-        instruction_idx: usize,
-        instruction_error: InstructionError,
-    },
+    #[error("Failed to decode the .text section")]
+    InstructionDecoderError(#[source] InstructionDecodeError),
+    #[error("Kernel error")]
+    KernelError(#[source] KernelError),
 }
 
-fn main() -> Result<(), RunError> {
+fn main() -> Result<(), ShellError> {
     let args = Args::parse();
     let path = std::path::PathBuf::from(args.path);
-    let file_data = std::fs::read(path).map_err(RunError::Load)?;
+    let file_data = std::fs::read(path).map_err(ShellError::Load)?;
     let slice = file_data.as_slice();
-    let file = ElfBytes::<AnyEndian>::minimal_parse(slice).map_err(RunError::ElfHead)?;
+    let file = ElfBytes::<AnyEndian>::minimal_parse(slice).map_err(ShellError::ElfHead)?;
 
     if file.ehdr.endianness != AnyEndian::Little {
-        return Err(RunError::ElfNotLittleEndian);
+        return Err(ShellError::ElfNotLittleEndian);
     }
 
     let text_header = file
         .section_header_by_name(".text")
-        .map_err(RunError::ElfSection)?
-        .ok_or(RunError::NoText)?;
+        .map_err(ShellError::ElfSection)?
+        .ok_or(ShellError::NoText)?;
     let (text, compression_header) = file
         .section_data(&text_header)
-        .map_err(RunError::TextData)?;
+        .map_err(ShellError::TextData)?;
     if compression_header.is_some() {
-        return Err(RunError::CompressedText);
+        return Err(ShellError::CompressedText);
     }
-    let program = text
+    if text.len() % 4 != 0 {
+        return Err(ShellError::UnalignedText);
+    }
+
+    let raw_stream = text
         .chunks(4)
-        .enumerate()
-        .map(parse_text_chunk)
-        .collect::<Result<Vec<_>, _>>()?;
+        .map(|x| <[u8; 4]>::try_from(x).unwrap())
+        .map(u32::from_le_bytes);
+    let program =
+        Program::from_raw_instructions(raw_stream).map_err(ShellError::InstructionDecoderError)?;
 
-    let text_offset = text_header.sh_addr;
-    let start = file.ehdr.e_entry;
-    let mut processor = Processor::new();
-
-    println!("text_offset={text_offset}, start={start}");
-
-    processor.pc = start;
+    let mut kernel = Kernel::new(program, file.ehdr.e_entry, text_header.sh_addr);
     for _ in 0..MAX_STEPS {
-        let instruction_idx = instruction_idx(processor.pc, text_offset);
-        println!(
-            "Fetching at {} (instruction {})",
-            processor.pc, instruction_idx
-        );
-
-        let instr = program
-            .get(instruction_idx)
-            .ok_or(RunError::FetchOutOfRange(instruction_idx))?;
-        println!("Executing: {instr:?}");
-
-        instr
-            .execute(&mut processor)
-            .map_err(|instruction_error| RunError::InstructionError {
-                instruction_idx,
-                instruction_error,
-            })?;
-        processor.pc += 4;
-
-        println!("New state: {processor:?}");
+        match kernel.step() {
+            Err(e) => return Err(ShellError::KernelError(e)),
+            Ok(s) => println!("{s:?}"),
+        }
     }
 
     Ok(())
-}
-
-fn parse_text_chunk((instruction_n, chunk): (usize, &[u8])) -> Result<Instruction, RunError> {
-    let instruction_bytes: [u8; 4] = chunk.try_into().map_err(|_| RunError::UnalignedText)?;
-    let instruction_val = u32::from_le_bytes(instruction_bytes);
-
-    decode_instruction(instruction_val).map_err(|reason| RunError::BadInstruction {
-        reason,
-        instruction_n,
-        instruction_val,
-    })
-}
-
-fn instruction_idx(pc: u64, text_offset: u64) -> usize {
-    // TODO: handle underflow
-    let local_offset = pc - text_offset;
-    (local_offset / 4) as usize
 }
